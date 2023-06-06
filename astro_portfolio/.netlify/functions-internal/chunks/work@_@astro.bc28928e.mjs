797986dc1146044f9ab80d841f16790ef1a90287
Dylan Abbott <dylanabbott33@yahoo.com>
2023-06-06 14:56:17 -0600

    something push

---
+++ astro_portfolio/.netlify/functions-internal/chunks/work@_@astro.bc28928e.mjs
@@ -0,0 +1,14 @@
+export { renderers } from '../renderers.mjs';
+import './astro.5ff3c2d5.mjs';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'slash';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'html-escaper';
+
+const page = () => import('./pages/work.astro.775408e7.mjs');
+
+export { page };
